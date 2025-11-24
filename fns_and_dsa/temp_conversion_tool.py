3a6043f00bb9e60f07179a9b2e781cd77632e2ef
Ayodele Ajisegiri <ayodeleajisegiri@gmail.com>
2025-11-24 02:17:53 +0100

    correction

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,8 +4,8 @@ Module for temperature conversion using global variables.
 """
 
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 
 def convert_to_celsius(fahrenheit):
